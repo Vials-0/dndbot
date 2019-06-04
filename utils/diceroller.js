651800dd4007a01acec464ce7704e9cc2f412449
Bradley_Carson <bmcarson1671@gmail.com>
2019-06-03 23:02:44 -0400

    Add linting rules via eslint

--- utils/diceroller.js
+++ utils/diceroller.js
@@ -1,23 +1,24 @@
 const rollDice = (number, sides) => {
-    try {
-        if (!Number.isInteger(number) || !Number.isInteger(sides)) {
-            throw "Arguments must be whole numbers!";
-        }
+	try {
+		if (!Number.isInteger(number) || !Number.isInteger(sides)) {
+			throw "Arguments must be whole numbers!";
+		}
 
-        const results = [];
-    
-        while (number > 0) {
-            let roll = Math.floor(Math.random() * sides) + 1;
-            results.push(roll);
-            number--;
-        }
+		const results = [];
 
-        if (number === 0) {
-            return results;
-        }
-    } catch (err) {
-        return err;
-    }
-}
+		while (number > 0) {
+			const roll = Math.floor(Math.random() * sides) + 1;
+			results.push(roll);
+			number--;
+		}
+
+		if (number === 0) {
+			return results;
+		}
+	}
+	catch (err) {
+		return err;
+	}
+};
 
 module.exports = rollDice;
